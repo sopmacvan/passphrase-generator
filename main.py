0ad7e1d049e84a9ee3aa057b1b5a1d73b1c295a4
R_Van <reijhxvan.campos@gmail.com>
2022-11-19 18:44:31 +0800

    added main features

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
+from passphrase import generate_passphrase, copy_passphrase
 import PySimpleGUI as sg
-
-# create customize description (left part)
-customize_description = [
-    [sg.Text('Set word count')],
-    [sg.Text('Set delimiter')],
-    [sg.Text('Include number')],
-    [sg.Text('Include Uppercase')]
-]
-
-# create customize input (right part)
-customize_input = [
-    [sg.Input(size=(2, 1))],
-    [sg.Input(size=(2, 1))],
-    [sg.Checkbox('', default=True)],
-    [sg.Checkbox('', default=True)],
-
-]
-
-# Create customize frame w/c contains both description and input
-customize_frame = sg.Frame(title='Customize',
-                           layout=[[sg.Column(customize_description), sg.Column(customize_input)]],
-                           expand_x=True)
 
 # Define the window's layout
 layout = [
-    [sg.Text("My-Generated-Passphrase1")],
-    [sg.Button('Generate Passphrase')],
-    [sg.Button('Copy Passphrase')],
+    [sg.Text(generate_passphrase(), key='-PASSPHRASE-')],
+    [sg.Button('Generate Passphrase', key='-GENERATE-')],
+    [sg.Button('Copy Passphrase', key='-COPY-')],
 
-    [customize_frame],
+    [sg.Frame(title='Customize',
+              layout=[[sg.Column([
+                  [sg.Text('Set word count')],
+                  [sg.Text('Set delimiter')],
+                  [sg.Text('Include number')],
+                  [sg.Text('Include Uppercase')]
+              ]), sg.Column([
+                  [sg.Input(size=(2, 1), default_text='3', key='-WORD COUNT-')],
+                  [sg.Input(size=(2, 1), default_text='-', key='-DELIMITER-')],
+                  [sg.Checkbox('', default=True, key='-INCLUDE NUMBER-')],
+                  [sg.Checkbox('', default=True, key='-INCLUDE UPPERCASE-')],
+
+              ])]],
+              expand_x=True)],
 
     [sg.Text('Password History')],
 
@@ -43,6 +35,21 @@ while True:
     # See if user wants to quit or window was closed
     if event in (sg.WINDOW_CLOSED, 'Quit'):
         break
+
+    # Generate a passphrase if user clicks the generate button
+    if event == '-GENERATE-':
+        n = int(values['-WORD COUNT-'])
+        delimiter = values['-DELIMITER-']
+        include_number = values['-INCLUDE NUMBER-']
+        include_uppercase = values['-INCLUDE UPPERCASE-']
+        passphrase = generate_passphrase(n, delimiter, include_number, include_uppercase)
+
+        window['-PASSPHRASE-'].update(passphrase)
+
+    # Copy the passphrase if user clicks the copy button
+    if event == '-COPY-':
+        passphrase = layout[0][0].DisplayText
+        copy_passphrase(passphrase)
 
 # Close the window
 window.close()
---
+++ passphrase.py
@@ -0,0 +1,49 @@
+from secrets import choice
+import string
+import win32clipboard
+
+
+def generate_passphrase(n=3, delimiter='-', include_number=True, include_uppercase=True):
+    """
+    Create a passphrase with n number of words,
+    separated by a delimiter, w/ or w/o a number and uppercase.
+
+    :param n: int
+    :param delimiter: str
+    :param include_number: bool
+    :param include_uppercase: bool
+    :return: str
+    """
+    with open('wordlists/eff_large_wordlist.txt_new') as f:
+        # get all words from wordlist
+        words = [word.strip() for word in f]
+
+        # choose n number of words
+        passphrase = [choice(words) for _ in range(n)]
+
+        # add a digit at the end of one random word if true
+        if include_number:
+            passphrase[choice(range(n))] += choice(string.digits)
+
+        # put delimiter
+        passphrase = delimiter.join(passphrase)
+
+        # set first letter of each word to uppercase if true
+        if include_uppercase:
+            passphrase = passphrase.title()
+
+        print(passphrase)
+        return passphrase
+
+
+def copy_passphrase(text):
+    """
+    Copy passphrase to clipboard.
+
+    :param text:
+    :return:
+    """
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardText(text, win32clipboard.CF_UNICODETEXT)
+    win32clipboard.CloseClipboard()
